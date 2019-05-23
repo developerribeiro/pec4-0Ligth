2140a745734b17096b4de15f58cb2d9aa99a4f79
Thiago Ribeiro <developerribeiro@gmail.com>
2019-05-22 21:04:51 -0300

    Add project files.

---
+++ Pec4.0Ligth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pec4._0Ligth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
